b85bce381d7db20976234cbe4cf4e597fe14659c
shanky-bhilwar <shankybhilwar24@gmail.com>
2025-07-03 14:17:53 +0530

    CandidateRegistration.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CandidateRegistration from './pages/CandidateRegistration' 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <CandidateRegistration/>
+    </div>
   )
 }
 
